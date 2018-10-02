9e940394312e27fe39398d11964e110fe7941be7
AdityaSrikanth <aditya.srikanth@relevancelab.com>
2018-10-02 23:23:40 +0530

    Added 2file

--- 2.py
+++ 2.py
@@ -2,5 +2,5 @@ import os
 
 os.system('git add .' )
 os.system('git commit -m "Added 2file"')
-os.system("git push https://AdityaSrikanth:oginadi7a@Terraform.git --all")
+os.system("git push https://AdityaSrikanth:Loginadi7a@Terraform.git --all")
 
