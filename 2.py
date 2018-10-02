398ea4ffbbf9f552473dabcbbfefd14da62bcc63
AdityaSrikanth <aditya.srikanth@relevancelab.com>
2018-10-02 23:58:31 +0530

    Added 2file

--- 2.py
+++ 2.py
@@ -9,6 +9,11 @@ os.system('git add .' )
 os.system('git commit -m "Added 2file"')
 # os.system('$Username = "AdityaSrikanth"')
 os.system('git config credential.helper store')
-os.system('git push https://github.com/AdityaSrikanth/Terraform.git')
-os.system("echo AdityaSrikanth")
+cmd = os.system('git push https://github.com/AdityaSrikanth/Terraform.git')
+child_process = popen_spawn.PopenSpawn(cmd)
+child_process.expect('User')
+child_process.sendline(user)
+child_process.expect('Password')
+child_process.sendline(password)
+
 
