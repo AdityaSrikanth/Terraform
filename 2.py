b323f2c9add23dad9a2cf8f25209a644fc03a7ee
AdityaSrikanth <aditya.srikanth@relevancelab.com>
2018-10-03 00:25:37 +0530

    Added 2file

--- 2.py
+++ 2.py
@@ -1,6 +1,5 @@
 import os
 
-from pexpect import popen_spawn
 
 user = 'AdityaSrikanth'
 password = 'Loginadi@7a'
@@ -8,12 +7,6 @@ password = 'Loginadi@7a'
 os.system('git add .' )
 os.system('git commit -m "Added 2file"')
 # os.system('$Username = "AdityaSrikanth"')
-os.system('git config credential.helper store')
-cmd = os.system('git push https://github.com/AdityaSrikanth/Terraform.git')
-child_process = popen_spawn.PopenSpawn(cmd)
-child_process.expect('User')
-child_process.sendline(user)
-child_process.expect('Password')
-child_process.sendline(password)
+os.system('AdityaSrikanth' | 'git push origin master')
 
 
