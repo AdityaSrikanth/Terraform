c49a074472e26bfe1ae0b9f9f2679543a7e20297
AdityaSrikanth <aditya.srikanth@relevancelab.com>
2018-10-03 00:34:41 +0530

    Added 2file

--- 2.py
+++ 2.py
@@ -7,6 +7,6 @@ password = 'Loginadi@7a'
 os.system('git add .' )
 os.system('git commit -m "Added 2file"')
 # os.system('$Username = "AdityaSrikanth"')
-os.system('git push origin master | AdityaSrikanth')
+os.system('git push origin master' | '$Username')
 
 
