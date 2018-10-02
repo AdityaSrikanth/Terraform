fab4f9d8565f73bbcc4b9d420229fbba05fe13a6
AdityaSrikanth <aditya.srikanth@relevancelab.com>
2018-10-02 23:38:48 +0530

    Added 2file

--- 2.py
+++ 2.py
@@ -7,4 +7,5 @@ os.system('git commit -m "Added 2file"')
 # os.system('$Username = "AdityaSrikanth"')
 os.system('git config credential.helper store')
 os.system('git push https://github.com/AdityaSrikanth/Terraform.git')
+os.system("echo AdityaSrikanth")
 
