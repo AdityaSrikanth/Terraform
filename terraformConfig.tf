fb1b69dc41ebec618ec25580e2fbbe1090bf2a5a
AdityaSrikanth <aditya.srikanth@relevancelab.com>
2018-10-03 14:13:49 +0530

    Update terraformConfig.tf

--- terraformConfig.tf
+++ terraformConfig.tf
@@ -1,29 +1,22 @@
-# # Configure the AWS Provider
-#                             variable "access_key"  {}
-#                             variable "secret_key" {}
-#                             provider "aws" {
-#                             access_key = "${var.access_key}"
-#                             secret_key = "${var.secret_key}"
-#                             region     = "us-east-2"
-#                                             }
+# Configure the AWS Provider
+                            variable "access_key"  {}
+                            variable "secret_key" {}
+                            provider "aws" {
+                            access_key = "${var.access_key}"
+                            secret_key = "${var.secret_key}"
+                            region     = "us-east-2"
+                                            }
                             
-#                         data "aws_instances" "test" {
-#                         filter {
-#                                 name   = "instance-state-name"
-#                                 values = ["running"]
-#                                 }
-#                         instance_state_names = [ "running", "stopped" ]
-#                                 }
+                        data "aws_instances" "test" {
+                        filter {
+                                name   = "instance-state-name"
+                                values = ["running"]
+                                }
+                        instance_state_names = [ "running", "stopped" ]
+                                }
 
-#                         resource "aws_eip" "test" {
-#                         count = "${length(data.aws_instances.test.ids)}"
-#                         instance = "${data.aws_instances.test.ids[count.index]}"
-#                         }
-data "http" "example" {
-  url = "https://www.google.co.in/"
+                        resource "aws_eip" "test" {
+                        count = "${length(data.aws_instances.test.ids)}"
+                        instance = "${data.aws_instances.test.ids[count.index]}"
+                        }
 
-  # Optional request headers
-  request_headers {
-    "Accept" = "application/json"
-  }
-}
